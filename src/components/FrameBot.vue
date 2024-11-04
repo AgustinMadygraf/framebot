24751c98946ce2435df7bb287b1d9c6135289339
AgustinMadygraf <agustin.mtto.madygraf@gmail.com>
2024-11-04 17:30:54 -0300

    refactor: enhance FrameBot layout and remove HelloWorld component

--- src/components/FrameBot.vue
+++ src/components/FrameBot.vue
@@ -1,10 +1,10 @@
 <template>
-    <div>
+    <div class="container">
       <!-- Iframe de ejemplo -->
-      <iframe src="http://localhost:8000" width="100%" height="200px"></iframe>
+      <iframe src="http://localhost:8000" class="iframe"></iframe>
       
       <!-- Chatbot -->
-      <div>
+      <div class="chatbot">
         <h3>Chat con MadyBotPy</h3>
         <input v-model="userMessage" placeholder="Escribe un mensaje" />
         <button @click="sendMessage">Enviar</button>
@@ -31,7 +31,7 @@
         try {
           const response = await axios.post('http://127.0.0.1:5000/receive-data', {
             message: this.userMessage,
-            user_id: '12345' // O el ID que quieras utilizar
+            user_id: '12345'
           });
           this.responseMessage = response.data.received_message;
         } catch (error) {
@@ -43,29 +43,23 @@
   </script>
   
   <style scoped>
-  iframe {
+  /* Contenedor principal que ocupa el 100% de la altura */
+  .container {
+    display: flex;
+    align-items: flex-start;
+    gap: 20px;
+    height: 100%; /* Hace que ocupe toda la altura disponible */
+  }
+  
+  /* Estilos para el iframe y el chatbot */
+  .iframe {
+    width: 80%;
+    height: 98vh;
     border: none;
   }
-  h3 {
-    font-family: Arial, sans-serif;
-    color: #333;
-  }
-  input {
-    padding: 8px;
-    margin-right: 4px;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-  }
-  button {
-    padding: 8px;
-    background-color: #4CAF50;
-    color: white;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-  }
-  button:hover {
-    background-color: #45a049;
+  .chatbot {
+    width: 20%;
+    height: 98vh;
   }
   </style>
   
