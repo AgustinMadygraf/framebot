09a6cb9b0f17348994500f7f76eae478107aa800
AgustinMadygraf <agustin.mtto.madygraf@gmail.com>
2024-11-05 20:36:01 -0300

    docstring

--- src/components/FrameBot.js
+++ src/components/FrameBot.js
@@ -9,13 +9,19 @@ export default {
     };
   },
   methods: {
+    /**
+     * Sends a message using the MessageService and updates the responseMessage.
+     * Logs the process of sending the message.
+     */
     async sendMessage() {
       console.log("Iniciando proceso de envío de mensaje...");
 
       try {
         this.responseMessage = await MessageService.sendMessage(this.userMessage);
+        console.log("Mensaje enviado exitosamente:", this.responseMessage);
       } catch (error) {
         this.responseMessage = error.message;
+        console.error("Error al enviar el mensaje:", error.message);
       } finally {
         console.log("Operación de envío de mensaje finalizada.");
       }
--- src/services/MessageService.js
+++ src/services/MessageService.js
@@ -2,12 +2,21 @@
 import ApiService from './ApiService';
 
 class MessageService {
+  /**
+   * Sends a message using the ApiService.
+   * @param {string} userMessage - The message to be sent.
+   * @returns {Promise<string>} - The response from the ApiService.
+   * @throws {Error} - Throws an error if the message could not be sent.
+   */
   async sendMessage(userMessage) {
+    console.log("Iniciando el envío del mensaje:", userMessage);
+
     try {
       const response = await ApiService.sendMessage(userMessage, '12345');
+      console.log("Mensaje enviado exitosamente:", response);
       return response;
     } catch (error) {
-      console.error(error.message);
+      console.error("Error al enviar el mensaje:", error.message);
       throw new Error("Hubo un problema al enviar el mensaje. Inténtalo de nuevo.");
     }
   }
