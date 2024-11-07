1ceaf6a72480765272d74d5be45b2c8817f5491a
AgustinMadygraf <agustin.mtto.madygraf>
2024-11-07 16:07:27 -0300

    feat: update MadyBot_Vue styles; add transparent background and restructure card layout

--- src/components/MadyBot_Vue.vue
+++ src/components/MadyBot_Vue.vue
@@ -4,20 +4,17 @@ Este archivo es el componente Vue que se encarga de mostrar el chatbot en la int
 -->
 <template>
   <div class="container">
-    <!-- Iframe -->
     <iframe src="http://127.0.0.1:8000" class="iframe"></iframe>
-    
-    <!-- Chatbot -->
     <div class="chatbot card">
-      <h3 class="card-header">MadyBot</h3>
+      <div class="card-header">
+        <h3>MadyBot</h3>
+      </div>
       <div class="card-body">
-        <div v-if="responseMessage" class="mt-3">
-          <p class="message-received"><strong>MadyBot:</strong> {{ responseMessage }}</p>
-        </div>
-        <input v-model="userMessage" placeholder="Escribe un mensaje" class="form-control mb-2" />
-        <div class="button-right">
-          <button @click="sendMessage" class="btn btn-primary">Enviar</button>
-        </div>
+          <p class="message-received">{{ responseMessage }}</p>
+      </div>
+      <div class="card-fotter">
+        <input v-model="userMessage" placeholder="Escribe un mensaje" class="form-control mb-2" />        
+        <button @click="sendMessage" class="btn btn-primary">Enviar</button>
       </div>
     </div>
   </div>
