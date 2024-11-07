ce83a88bbd08c3f6141e0af8f3da34131e2bedcc
AgustinMadygraf <agustin.mtto.madygraf>
2024-11-07 07:30:07 -0300

    feat: update MadyBot_Vue component styles; add overflow handling and adjust message display

--- src/components/MadyBot_Vue.vue
+++ src/components/MadyBot_Vue.vue
@@ -8,7 +8,7 @@
       <h3 class="card-header">MadyBot</h3>
       <div class="card-body">
         <div v-if="responseMessage" class="mt-3">
-          <p class="message-received"><strong>MadyBotPy:</strong> {{ responseMessage }}</p>
+          <p class="message-received"><strong>MadyBot:</strong> {{ responseMessage }}</p>
         </div>
         <input v-model="userMessage" placeholder="Escribe un mensaje" class="form-control mb-2" />
         <button @click="sendMessage" class="btn btn-primary">Enviar</button>
