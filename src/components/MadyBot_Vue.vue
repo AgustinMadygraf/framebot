1087ef8cd800830498156a400ccdaa025291a787
AgustinMadygraf <agustin.mtto.madygraf>
2024-11-06 21:49:42 -0300

    feat: integrate Bootstrap and Bootstrap-Vue; update MadyBot_Vue component styles and structure

--- src/components/MadyBot_Vue.vue
+++ src/components/MadyBot_Vue.vue
@@ -1,25 +1,26 @@
 <template>
-    <div class="container">
-      <!-- Iframe de ejemplo -->
-      <iframe src="http://127.0.0.1:8000" class="iframe"></iframe>
-      
-      <!-- Chatbot -->
-      <div class="chatbot">
-        <h3>MadyBot</h3>
-        <input v-model="userMessage" placeholder="Escribe un mensaje" />
-        <button @click="sendMessage">Enviar</button>
+  <div class="container">
+    <!-- Iframe de ejemplo -->
+    <iframe src="http://127.0.0.1:8000" class="iframe"></iframe>
+    
+    <!-- Chatbot -->
+    <div class="chatbot card">
+      <h3 class="card-header">MadyBot</h3>
+      <div class="card-body">
+        <input v-model="userMessage" placeholder="Escribe un mensaje" class="form-control mb-2" />
+        <button @click="sendMessage" class="btn btn-primary">Enviar</button>
         
-        <div v-if="responseMessage">
+        <div v-if="responseMessage" class="mt-3">
           <p><strong>MadyBotPy:</strong> {{ responseMessage }}</p>
         </div>
       </div>
     </div>
-  </template>
-  
-  <script>
-  import MadyBot_VueScript from './MadyBot_Vue.js'; // Importa el script externo
-  import './MadyBot_Vue.css';  // Importa el archivo CSS
-  
-  export default MadyBot_VueScript;
-  </script>
-  
+  </div>
+</template>
+
+<script>
+import MadyBot_VueScript from './MadyBot_Vue.js'; // Importa el script externo
+import './MadyBot_Vue.css';  // Importa el archivo CSS
+
+export default MadyBot_VueScript;
+</script>
